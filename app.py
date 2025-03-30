d901c29cb8b1457babbd629333ed62d5c19774e2
Suzane <suzaane97@gmail.com>
2025-03-30 20:17:03 -0300

    API livros GET

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, jsonify  # Importamos a classe Flask do módulo flask para criar nosso aplicativo web
 import sqlite3
+from flask_cors import CORS
+
 
 # Aqui estamos criando uma instância do Flask e armazenando na variável "app"
 # O parâmetro __name__ é passado para o Flask para que ele consiga identificar o arquivo principal da aplicação
 app = Flask(__name__)
+CORS(app)
 
 # Aqui estamos criando uma rota para o endpoint "/pagar"
 # Ou seja, quando acessarmos http://127.0.0.1:5000/pagar no navegador, a função abaixo será executada
-@app.route("/pagar")
-def exibir_mensagem():
-    # Retorna um texto formatado em HTML para ser exibido na página da rota "/pagar"
-    return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
+# @app.route("/pagar")
+# def exibir_mensagem():
+#     # Retorna um texto formatado em HTML para ser exibido na página da rota "/pagar"
+#     return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
 
-# Criamos outra rota para o endpoint "/femandaopix"
-# Quando acessarmos http://127.0.0.1:5000/femandaopix, a função será chamada automaticamente
+# # Criamos outra rota para o endpoint "/femandaopix"
+# # Quando acessarmos http://127.0.0.1:5000/femandaopix, a função será chamada automaticamente
 @app.route("/")
 def manda_o_pix():
     # Retorna um texto formatado em HTML que será exibido no navegador
     return "<h2>SE TEM DOR DE CUTUVELO, TÁ DEVENDO</h2>"
 
-# Criamos uma terceira rota para o endpoint "/comida"
-# Sempre que o usuário acessar http://127.0.0.1:5000/comida, essa função será executada
-@app.route("/comida")
-def comida():
-    # Retorna um texto formatado em HTML com uma mensagem sobre comida
-    return "<h2>Tomato à milanesa</h2>"
+# # Criamos uma terceira rota para o endpoint "/comida"
+# # Sempre que o usuário acessar http://127.0.0.1:5000/comida, essa função será executada
+# @app.route("/comida")
+# def comida():
+#     # Retorna um texto formatado em HTML com uma mensagem sobre comida
+#     return "<h2>Tomato à milanesa</h2>"
 
 # iniciando o banco de dados
 def init_db():
@@ -70,6 +73,29 @@ def doar():
 
 
     return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+
+@app.route("/livros", methods=["GET"])
+def listar_livros():
+    with sqlite3.connect("database.db") as conn:
+        livros = conn.execute("SELECT * FROM LIVROS").fetchall() # fetchall() traduz a linguagem sql para python
+
+        # Aqui estamos criando uma lista de dicionários, onde cada dicionário representa um livro
+        lista_formatados = []
+
+        for item in livros:
+            dicionario_livros = {
+                "id": item[0],
+                "titulo": item[1],
+                "categoria": item[2],
+                "autor": item[3],
+                "imagem_url": item[4]
+            }
+
+            lista_formatados.append(dicionario_livros)
+
+            
+    return jsonify(lista_formatados), 200
 
 
 # Aqui verificamos se o script está sendo executado diretamente e não importado como módulo
